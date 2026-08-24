b98540f443751cda5b1ab5125816e079c6c4e7fb
ramiseneka-boop <ramiseneka@gmail.com>
2026-08-24 12:36:58 +0500

    Add Software Auction CRM route

---
+++ app/auction/page.tsx
@@ -0,0 +1,10 @@
+import AuctionApp from "@/components/auction/AuctionApp";
+
+export const metadata = {
+  title: "Software Auction CRM",
+  description: "Личная CRM для рынка-аукциона дорогого B2B SaaS",
+};
+
+export default function SoftwareAuctionPage() {
+  return <AuctionApp />;
+}
